9b38fd1503f7abad2f85fe20add348d20c00e886
qubit-quilt-agent <michaelruelas+githubagent@gmail.com>
2025-10-11 20:06:25 +0000

    Add Next server polyfills and NextResponse mock for tests; wire into jest setup
    Co-authored-by: qubit-quilt-agent <michaelruelas+githubagent@gmail.com>

---
+++ jest.setup.ts
@@ -0,0 +1,49 @@
+import '@testing-library/jest-dom'
+
+// Polyfill minimal web globals required by Next server modules
+// Provide Request/Response/Headers so server route modules can import next/server
+// These are lightweight stand-ins for testing purposes.
+
+if (typeof global.Request === 'undefined') {
+  // minimal Request polyfill
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  global.Request = class Request {
+    constructor(input: any, init: any) {
+      this.input = input
+      this.init = init
+    }
+  }
+}
+if (typeof global.Response === 'undefined') {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  global.Response = class Response {
+    constructor(body: any, init: any) {
+      this.body = body
+      this.init = init
+    }
+  }
+}
+if (typeof global.Headers === 'undefined') {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  global.Headers = class Headers {
+    constructor(init: any) {
+      this.map = init || {}
+    }
+  }
+}
+
+// Mock next/server NextResponse.json used by route handlers
+// Ensure NextResponse.json returns a plain object for assertions
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+global.NextResponse = {
+  json: (body: any, init?: any) => ({ body, status: init?.status ?? 200 }),
+}
+
+// Provide a default mock for getServerSession from next-auth
+// so routes that import it won't throw; individual tests can override this mock.
+
+jest.mock('next-auth', () => ({ getServerSession: jest.fn() }))
