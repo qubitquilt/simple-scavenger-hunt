b804c807e8752cc65b50e7b4ef6779136664e2e4
qubit-quilt-agent <michaelruelas+githubagent@gmail.com>
2025-10-11 20:05:26 +0000

    Polyfill minimal Request/Response/Headers in test setup for Next server imports
    Co-authored-by: qubit-quilt-agent <michaelruelas+githubagent@gmail.com>

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -1,1 +1,36 @@
 import '@testing-library/jest-dom'
+
+// Polyfill minimal web globals required by Next server modules
+// Provide Request/Response/Headers so server route modules can import next/server
+// These are lightweight stand-ins for testing purposes.
+
+if (typeof global.Request === 'undefined') {
+  // minimal Request polyfill
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  global.Request = class Request {
+    constructor(input: any, init: any) {
+      this.input = input
+      this.init = init
+    }
+  }
+}
+if (typeof global.Response === 'undefined') {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  global.Response = class Response {
+    constructor(body: any, init: any) {
+      this.body = body
+      this.init = init
+    }
+  }
+}
+if (typeof global.Headers === 'undefined') {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  global.Headers = class Headers {
+    constructor(init: any) {
+      this.map = init || {}
+    }
+  }
+}
